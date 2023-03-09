c2d5d5c1ea8df89ff08e5c279f62bd3eec9aa8d3
George T Chakama <37905683+gtchakama@users.noreply.github.com>
2023-03-09 22:24:49 +0200

    Update Question.js

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,16 +1,47 @@
-import React from 'react';
+import React from "react";
 
 export default function Question(props) {
-    
-    console.log(props.question);
-    return (
-        <div className='question'>
-            {props.question}
-            <div className="options">
-                
-            </div>
-        </div>
-    )
-    
-}
+  const { answer, wrongAnswers, difficulty, question, Number } = props;
 
+  const AnswerOptions = [answer, ...wrongAnswers];
+
+  var QuestionObject = {
+    Options: AnswerOptions,
+    Answer: answer,
+    Difficulty: difficulty,
+    Question: question,
+    Index: Number + 1,
+  };
+
+  const checkAnswer = (chosenAnswer) => {
+    if (chosenAnswer === QuestionObject.Answer) {
+      console.log("Correct Answer");
+    } else {
+      console.log("Wrong Answer");
+    }
+  };
+
+  return (
+    <div className="question" key={QuestionObject.Index}>
+      <h1>
+        <span>{QuestionObject.Index}</span>
+
+        {QuestionObject.Question}
+      </h1>
+
+      <div className="options">
+        {QuestionObject.Options.map((option) => {
+          return (
+            <button
+              onClick={() => {
+                checkAnswer(option);
+              }}
+            >
+              {option}
+            </button>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
